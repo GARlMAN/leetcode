34046b6497ee5e7437dead17f405e79640f14e06
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2022-11-10 16:00:00 +0530

    Time: 39 ms (68.66%), Space: 11 MB (48.01%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
-        stack <char> st;
-        for(int i = 0; i < s.size(); ++i){
-            if(!st.empty() && st.top() == s[i]){
-                st.pop();
-            }
-            else{
-                st.push(s[i]);
-            }
-        }
-        string ans = "";
-        while(!st.empty()){
-            ans =  st.top() + ans;
-            st.pop();
-        }
-        return ans;
+    string removeDuplicates(string S) {
+        string res = "";
+        for (char& c : S)
+            if (res.size() && c == res.back())
+                res.pop_back();
+            else
+                res.push_back(c);
+        return res;
     }
 };
