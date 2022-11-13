037cd0907461ee4f2885244c6f51f57e961762f0
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2022-11-14 00:06:26 +0530

    Time: 4 ms (34.13%), Space: 6 MB (31.12%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[1001];
+    int solve(int n){
+        if(n == 1)
+            return 0;
+        if(dp[n] != -1)
+            return dp[n];
+        for(int i = 1; i < n; ++i){
+            if(n % i == 0){
+                if(solve(n - i) == 0){
+                    dp[n] = 1;
+                    return 1;
+                }
+                    
+            }
+        }    
+        dp[n] = 0;
+        return 0;
+    }
+    
+    
+    bool divisorGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+};
