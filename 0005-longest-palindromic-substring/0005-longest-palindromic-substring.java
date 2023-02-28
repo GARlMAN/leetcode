d67e963e56868ccd89b63de5ab56154a0d99623f
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-03-01 00:52:12 +0530

    Time: 885 ms (5.81%), Space: 235.5 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,15 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
+        
+        //dp array
          String dp[][] = new String[s.length()][s.length()];
         int ans_i = 0;
         int ans_j = 0;
+        
+        //dp array
         String ans = "";
+        
+        
         for(int k = 0; k < dp.length; ++k){
+            
+            //loops over filling half the dp array
             for(int i = 0, j = k; j < dp.length; j++, i++){
+                
+                //works for the "a" edge case
                 if(k == 0){
                     dp[i][j] = s.charAt(i) + "";
                 }
                 else if(k == 1){
+                    
+                    //worjs for the "BB" edge case
                     if(s.charAt(i) == s.charAt(j)){
                         dp[i][j] = "" + s.charAt(i) + "" + s.charAt(j) + "";
                          System.out.print( s.charAt(i) + "" + s.charAt(j) );
@@ -18,6 +30,8 @@ class Solution {
                         dp[i][j] = "";
                 }
                 else{
+                    
+                    //checks the previoly computed palindrome to check and use dp
                     if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] != "")
                         dp[i][j] = s.charAt(i) +  dp[i + 1][j - 1] + s.charAt(j);
                     else
@@ -27,6 +41,8 @@ class Solution {
                 ans = (dp[i][j]).length() > ans.length() ? dp[i][j] : ans;
             }
         }
+        
+        //print the array
         // for(int k = 0; k < dp.length; ++k){
         //     for(int i = 0; i < dp.length; ++i){
         //         System.out.print(dp[k][i] + " ");
