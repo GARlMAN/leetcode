31149eea8af9083a8bf6504cdbed24cfb56bb73b
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-03-23 15:54:28 +0530

    Time: 541 ms (65.36%), Space: 27.6 MB (32.50%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,4 +1,14 @@
 class Solution:
-    def findKthLargest(self, nums, k):
-        return sorted(nums, reverse=True)[k-1]
-            
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pivot = choice(nums)
+
+        greater = [x for x in nums if x > pivot]
+        equal = [x for x in nums if x == pivot]
+        less = [x for x in nums if x < pivot]
+
+        if k <= len(greater):
+            return self.findKthLargest(greater, k)
+        elif k > len(greater) + len(equal):
+            return self.findKthLargest(less, k - len(greater) - len(equal))
+        else:
+            return equal[0]
