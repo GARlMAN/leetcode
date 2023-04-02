2307dbf03e8ed50c32b8bc2c1f0fe233d15fc27b
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-02 23:51:00 +0530

    Time: 1091 ms (5.07%), Space: 394 MB (7.64%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minSideJumps(vector<int>& obstacles) {
+        //dp of the given array that takes in a pair
         map<pair<int,int>,int> dp;
         return solve(2, 0, obstacles, dp);
     }
@@ -8,24 +9,27 @@ public:
     
     int solve(int pos, int i, vector<int>& obstacles, map<pair<int,int>,int> &dp){
 
-               
+        //checks if the combinations exists       
         if(dp[{pos, i}])
             return dp[{pos, i}]; 
         int ans = INT_MAX;
         
-        
+        //goes in a straight lines till it finds a obstacle in the next tile
         for(int k = i; k < obstacles.size() - 1; k++){
+            //checks for obstackle
             if(pos == obstacles[k + 1]){
+                //checks all the jumps it can make if obstacle is there
                 for(int j = 1; j <= 3; ++j){
                     if(pos != j && j != obstacles[k])
                         ans = min(ans, 1 + solve(j, k, obstacles, dp));
                 }
+                //stores value in dp
                 dp[{pos, i}] = ans;
                 return ans;
             }            
         }
 
-            
+        //returns 0 if it no jumps are done    
         return 0;
     }
 };
