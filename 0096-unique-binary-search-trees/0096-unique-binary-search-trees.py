8d3b15519cc8dcf1b0bad4284e6bac8facae1a58
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-06 23:34:12 +0530

    Time: 34 ms (43.78%), Space: 13.8 MB (93.92%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,10 +1,13 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         dp = [-1] * (n + 1)
+        #starting value
         dp[0] = 1
         dp[1] = 1
+        #go through all numbers
         for i in range(2, n + 1):
             ans = 0
+            #use dp to mupltiply the product and add to ans to cal for int(i)
             for j in range(i):
                 ans += dp[j] * dp[i - j - 1]
             dp[i] = ans
