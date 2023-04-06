01a7401acb8935f06384b157d0491c16bfe58ecd
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-06 12:44:19 +0530

    Time: 44 ms (40.36%), Space: 28.2 MB (60.16%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-       
-        int n = nums.size();
         int ans = INT_MAX;
-        int i=0,j=0;
-        int sum=0;
-        while(i<n){
-            //if sum is less than target then continue adding it.
-            sum+=nums[i];
-            //if sum is >= target then as we have to find the minimum length subarray so we will check if by
-            //incrementing j,can we get required target and update ans.
-            //here ans = min(ans,(i-j+2)) bcoz by incremneting j if sum<target then we will not be able to achieve
-            //our requiered target so we need to include prev value also,so we (i-j+1)+1.
-            while(sum>=target){
-                sum-=nums[j];
-                j++;
-                ans=min(ans,(i-j+2));
+        int sum = 0;
+        int element = 0;
+        int i = 0;
+        while(i < nums.size()){
+            if(sum < target)
+                sum += nums[i];
+            while(sum >= target){
+                sum -= nums[element];
+                element++;
+                ans = min(ans, i - element + 2);
             }
             i++;
         }
-        return (ans!=INT_MAX)?ans:0;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
