e6c18418ae15c18f6c7150a720bb7287076cf786
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-07 23:35:47 +0530

    Time: 25 ms (18.20%), Space: 16.2 MB (50.17%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -13,30 +13,36 @@ class Solution {
 public:
     vector<TreeNode*> ans;
     vector<TreeNode*> generateTrees(int n) {
-        
+        //calling the solve fuction
         return solve(1, n);
     }
     
     vector<TreeNode*> solve(int start, int end){
+        //defining the values
         vector<TreeNode*> a;
+        //checking if r > l
         if(start > end){
             a.push_back(NULL);
             return a;
         }
-          
+        //going through values from start - end  
         for(int i = start; i <= end; ++i){
             vector<TreeNode*> left = solve(start, i - 1);
             vector<TreeNode*> right = solve(i + 1, end);
+            
+            //appending all the combinations of left to write with int(i)
             for(auto l : left){
                 for(auto r : right){
                     TreeNode* temp = new TreeNode;
                     temp->val = i;
                     temp->left = l;
                     temp->right = r;
+                    //appending the root to array
                     a.push_back(temp);
                 }
             }
         }
+        //returning the given vector
         return a;
     }
 };
