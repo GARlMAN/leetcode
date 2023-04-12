62e9aca8d67fa8218bc6bd3a4e12eacb527c31e7
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-12 14:16:14 +0530

    Time: 275 ms (14.53%), Space: 8.3 MB (52.28%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
-    int solve(int start,int end,vector<vector<int>>&dp){
-        if(start>=end){
-            return 0;
-        }
-
-        if(dp[start][end]!=-1){
-            return dp[start][end];
-        }
-
-        int ans=INT_MAX;
-
-        for(int i=start;i<=end;i++){
-            ans=min(ans,i+max(solve(start,i-1,dp),solve(i+1,end,dp)));
-        }
-        dp[start][end]=ans;
-        return ans;
-    }
 public:
+    
     int getMoneyAmount(int n) {
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(0,n,dp);
+        return solve(1, n, dp);
+    }
+    int solve(int start, int end, vector<vector<int>> &dp){
+        if(end <= start)
+            return 0;
+        if(dp[start][end]  != -1)
+            return dp[start][end] ;
+        
+            
+        int ans = INT_MAX;
+        for(int i = start; i <= end; ++i){
+            int left = i + solve(start, i - 1, dp);
+            int right = i + solve(i + 1, end, dp);
+            ans = min(ans, max(left, right));
+        }
+        dp[start][end]  = ans;
+        return ans;
     }
 };
