fc347474e20ec5d7b2617f062803c248d8866862
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-23 21:37:29 +0530

    Time: 31 ms (76.62%), Space: 21.4 MB (89.71%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[abs(nums[i]) - 1] > 0)
+                nums[abs(nums[i])- 1] = -nums[abs(nums[i]) - 1];
+            else
+                ans.push_back(abs(nums[i]));
+        }
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] > 0)
+                ans.push_back(i + 1);
+        }
+        return ans;
+        
+    }
+};
