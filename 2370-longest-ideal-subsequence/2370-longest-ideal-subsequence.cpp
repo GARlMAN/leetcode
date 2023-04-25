840224fbb3e69325d5961b523f8ff0952e1be67c
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-04-25 19:16:17 +0530

    Time: 66 ms (87.32%), Space: 10.3 MB (62.82%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -3,9 +3,13 @@ public:
     unordered_map<int, int> dp;
     int longestIdealString(string s, int k) {
         int dp[150] = {}, res = 0;
+        //maintaing a hash map isntead of going over all elements
         for (auto& i : s) {
+            
+            //checking only values in k range
             for (int j = i - k; j <= i + k; ++j)
                 dp[i] = max(dp[i], dp[j]);
+            //adding 1 to dp[i] and returning res which is the ans
             res = max(res, ++dp[i]);
         }
         return res;
