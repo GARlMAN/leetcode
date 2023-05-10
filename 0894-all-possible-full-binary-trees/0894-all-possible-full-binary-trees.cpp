e990b9ac250a323513a0df6c6e3cda2ba04b024f
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-10 18:40:56 +0530

    Time: 117 ms (34.12%), Space: 37.5 MB (26.49%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -13,17 +13,20 @@ class Solution {
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
         vector<TreeNode*> ans;
+        //all perfect binary trees should be odd
         if(n % 2 == 0)
             return ans;
+        //dp vector
         map <pair<int, int> , vector<TreeNode*>> dp;
         return solve(1, n, dp);
     }
-    
+    //solve function
     vector<TreeNode*> solve(int s, int e, map<pair<int, int> ,vector<TreeNode*>> dp){
         vector<TreeNode*> ans;
+        //if key already exists
         if (dp.find({s, e}) != dp.end())
             return dp[{s, e}];
-        
+        //returning leaf node value
         if(s == e){
             TreeNode* root = new TreeNode;
             root->val = 0;
@@ -37,9 +40,8 @@ public:
 
                 vector<TreeNode*> lefts = solve(s, i - 1, dp);
                 vector<TreeNode*> rights = solve(i + 1, e, dp);
-                
-                for(auto left : lefts){
-                    
+                //combination of all left and right nodess
+                for(auto left : lefts){                    
                     for(auto right : rights){
                         
                         TreeNode* root = new TreeNode;
@@ -54,6 +56,7 @@ public:
                 
             }
         }
+        //setting dp values and returning
         dp[{s, e}] = ans;
         return ans;
     }
