e235425efbf849d5945f88803c9241f508845e95
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-12 20:30:27 +0530

    Time: 23 ms (38.45%), Space: 20.1 MB (25.90%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        
+        //assing a dp array of n*n
         vector<vector<int>> dp(s.size(), vector<int> (s.size(), 1));
         int ans = s.size();
+        
+        
         for(int i = 1; i < s.size(); ++i){
             for(int row = 0, col = i; col < s.size(); ++col, row++){
+                //check for aa edge case
                 if(i == 1)
                     dp[row][col] = s[row] == s[col] ? 1 : 0;
+                //assingin alrady calculated value to find the palindrome
                 else
                     dp[row][col] = s[row] == s[col] ? dp[row + 1][col - 1] : 0;
-                ans += dp[row][col];
-                
-                
-                
-                
+                //adding to the answer
+                ans += dp[row][col];  
             }
-            
-            
-            
         }
         
         
