c76c2ae0139e72dca5b093baad66576a554d4e72
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-15 17:09:35 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (9.91%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -7,13 +7,14 @@ public:
         int current = 0;
         vector<int> values(s.size() + 1, 0);
         for(int i = 0; i < s.size(); ++i){
+            //pushing in stack if there's a open bracket
             if(s[i] == '(')
                 st.push(1);
             else{
-                // if(st.empty())
-                //     values[i] = 0;
                 if(!st.empty()){
+                    //checking for i - 1 value for (()) case and checking for values that would connect other bracket whcih is values[i - values[i]]
                     values[i] = 2 + values[i - 1];
+
                     if(i - values[i] >= 0)
                         values[i] += values[i - values[i]];
                     ans = max(ans, values[i]);
