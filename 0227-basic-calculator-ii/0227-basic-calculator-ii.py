f628e8c8544697c2e7e2bd4840803a1aac91662c
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-18 02:47:01 +0530

    Time: 132 ms (83.85%), Space: 18.7 MB (38.42%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,48 +2,49 @@ class Solution:
     def calculate(self, s: str) -> int:
         lis = []
         num = ""
-        if(len(s) == 299999):
-            return 2
+        prev_num = 0
+        current = ""
+        res = 0
+        operator = '+'
         for i in s:
             if(i == ' '):
                 continue
-            if not i. isdigit():
-                lis.append(num)
-                num = ""
-                lis.append(i)
+            if not i.isdigit():
+                current = int(current)
+                if operator == '+':
+                    prev_num = current
+                    res += int(current)
+                elif operator == '-':
+                    prev_num = -current
+                    res -= int(current)
+                elif operator == '*':
+                    res -= prev_num
+                    res += prev_num*current
+                    prev_num = prev_num*current
+                elif operator == '/':
+                    res -= prev_num
+                    res += int(prev_num/current)
+                    prev_num = int(prev_num/current)
+                operator = i
+                current = ""
             else:
-                num += i
-        lis.append(num)
-        i = 0
-        n = len(lis)
-        while i < len(lis) - 1:
-            if lis[i] == '*':
-                cal = int(lis[i - 1]) * int(lis[i + 1])
-                lis[i] = cal
-                lis.pop(i + 1)
-                lis.pop(i - 1)
-                i -= 1
-            elif lis[i] == '/':
-                cal = int(lis[i - 1]) / int(lis[i + 1])
-                lis[i] = cal
-                lis.pop(i + 1)
-                lis.pop(i - 1)
-                i -= 1
-            i = i + 1
-        i = 0  
-        while i < len(lis) - 1:
-            if lis[i] == '+':
-                cal = int(lis[i - 1]) + int(lis[i + 1])
-                lis[i] = cal
-                lis.pop(i + 1)
-                lis.pop(i - 1)
-                i -= 1
-            elif lis[i] == '-':
-                cal = int(lis[i - 1]) - int(lis[i + 1])
-                lis[i] = cal
-                lis.pop(i + 1)
-                lis.pop(i - 1)
-                i -= 1
-            i = i + 1
-        print(lis)
-        return int(lis[0])
+                current += i
+
+        current = int(current)        
+        if operator == '+':
+            prev_num = current
+            res += int(current)
+        elif operator == '-':
+            prev_num = -current
+            res -= int(current)
+        elif operator == '*':
+            res -= prev_num
+            res += prev_num*current
+            prev_num = prev_num*current
+        elif operator == '/':
+            res -= prev_num
+            res += int(prev_num/current)
+            prev_num = int(prev_num/current)
+        operator = i
+        current = ""
+        return res
