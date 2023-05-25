c55ffdddcdb9fe155baddf47f362694b1ee56339
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-26 00:13:24 +0530

    Time: 89 ms (61.05%), Space: 61.3 MB (24.53%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -3,6 +3,9 @@ public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int product = 1, count = 0, ans = 0, start = 0;
 
+        
+        //checking solution using two pointers my maintaint a count varaiable
+        //has a start that increments when the product exceeds k
         for(int i = 0; i < nums.size(); ++i){
             product *= nums[i];
             count++;
@@ -22,4 +25,4 @@ public:
         }
          return ans;
     }
-};;
+};
