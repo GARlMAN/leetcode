1d09a2b518202b8c7a2ef5e4cd84646dc8ecce67
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-05-25 16:06:52 +0530

    Time: 150 ms (60.54%), Space: 67.6 MB (90.26%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        vector<int> count;
+        int start = 0;
+        int count_odd = 0;
+        int count = 1;
         int ans = 0;
         for(int i = 0; i < nums.size(); ++i){
             if(nums[i] % 2 != 0)
-                count.push_back(i);
-        }
-        int size = count.size();
-//         if(count.size() - k <= 0)
-            // return 0;
-        for(int left = 0; left <= size - k; ++left){
-            int start = (count[left] == 0|| left == 0) ? 0 : count[left - 1] + 1;
-            int end = (count[left] == nums.size() - 1 || left == count.size() - k) ? nums.size() - 1 : count[left + k] - 1;
-            
-            
-            int n = count[left] + 1 - start;
-            int m = end - count[left + k - 1] + 1;
-            int cal = n * m;
-            cout << n << endl;
-            ans += cal;
-            
-            
-            
+                count_odd++;
+            if(count_odd > k){
+                count = 1;
+                start++;
+                count_odd--;
+            }
+            if(count_odd == k){
+
+                while(nums[start]%2 == 0){
+                    
+                    start++;
+                    count++;
+                }
+
+                ans += count;
+                
+            }
+                
         }
         return ans;
     }
