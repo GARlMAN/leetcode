3814621606d2ced98aeca9d926bcad88a49b142e
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-06-01 15:50:16 +0530

    Time: 62 ms (5.09%), Space: 10.7 MB (5.21%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -11,8 +11,10 @@ public:
         if(i == n)
             return b1 == b2 ? 1 : 0;
         long long  count = 0;
+        //dp return 
         if(dp[{{b1, b2}, i}])
             return dp[{{b1, b2}, i}];
+        
         if(b1 == 1 && b2 == 1)
             count += solve(0, 0, i + 1) % m;
         else if(b1 == 0 && b2 == 0)
@@ -21,10 +23,10 @@ public:
             count += solve(1, 1, i + 1)%m + solve(0, 1, i + 1)%m;
         else
             count += solve(1, 1, i + 1)%m + solve(1, 0, i + 1)%m;
+        
+        //storing values in dp based on gap and index
         dp[{{b1, b2}, i}] = count%m;
         return count%m;
             
-        
-        
     }
 };
