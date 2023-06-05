a213c0740755926d92a2d1e060a3ad4f984bc9a2
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-06-05 15:52:14 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (44.70%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -9,6 +9,7 @@ public:
             left = max(left, num);
         }
         int ans;
+        //binary search algo which checks if values for a given mid is possible or not if it's not then it changes the value of mid 
         while(left <= right){
             int mid = (left + right)/2;
             if(check(nums, mid, k)){
@@ -21,6 +22,7 @@ public:
         return ans;
     }
     bool check(vector<int>& nums, int mid, int k){
+        //this is to check how many sub-arrays can be formed with the mid value and see if it's greater than k
         int curr_sum = 0;
         int count = 1;
         for(auto num : nums){
