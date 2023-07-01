e3c40954ecee06e86a9f734d998c0d71d259eee7
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-07-01 22:35:46 +0530

    Time: 51 ms (25.32%), Space: 16.4 MB (38.00%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,25 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        hash_map = [0] * len(s)
-        def solve(j):
-            ans = ''
-            num = ''
-            for i in range(j, len(s)):
-                if(s[i].isdigit() and hash_map[i] != 1):
-                    hash_map[i] = 1
-                    num += s[i]
-                elif(s[i] == '[' and hash_map[i] != 1):
-                    hash_map[i] = 1
-                    ans += int(num) * solve(i + 1)
-                    num = ''
-                elif(s[i] == ']'):
-                    if(hash_map[i] != 1):
-                        hash_map[i] = 1
-                        return ans
-                    hash_map[i] = 1 
-                elif(s[i]. isalpha() and hash_map[i] != 1):
-                    hash_map[i] = 1
-                    ans += s[i]        
-            return ans 
-        
-        return solve(0) 
+        currString = ''
+        num = ''
+        stack = []
+        for i in s:
+            
+            if i == '[':
+                stack.append(currString)
+                stack.append(num)
+                currString = ''
+                num = ''
+            elif i == ']':
+                prevnum = stack.pop()
+                prevString = stack.pop()
+                currString =prevString + int(prevnum) * currString
+            elif i.isdigit():
+                num += i
+            else:
+                currString += i
+
+        return currString
