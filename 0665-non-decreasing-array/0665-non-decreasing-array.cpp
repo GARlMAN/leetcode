9b4c2b17115eb383b5120cf4fc001f4e5c30af2e
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-07-08 15:01:28 +0530

    Time: 31 ms (61.55%), Space: 26.9 MB (97.59%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -5,13 +5,13 @@ public:
         for(int i = 1; i < nums.size(); ++i){
 
             if(nums[i] < nums[i - 1]){
-            if(++count > 1)
-                return false;
-            if(i == 1 || nums[i - 2] <= nums[i])
-                nums[i - 1] = nums[i];
-            else
-                nums[i] = nums[i - 1];
-            }
+                if(++count > 1)
+                    return false;
+                if(i == 1 || nums[i - 2] <= nums[i])
+                    nums[i - 1] = nums[i];
+                else
+                    nums[i] = nums[i - 1];
+                }
 
         }
         
