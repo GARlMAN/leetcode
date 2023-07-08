bb720f90ca0b575101fd23b1cac04e237b58a65d
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-07-08 15:01:20 +0530

    Time: 39 ms (14.54%), Space: 26.9 MB (97.59%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        if(nums.size() <= 2)
-            return true;
         int count = 0;
-        int n = nums.size() - 1;
-        int max_ans = nums[0];
-        for(int i = 0; i <= n; ++i){
-            max_ans = max(max_ans, nums[i]);
-            if(max_ans > nums[i])
-                count++;
-        }
-        int ans = 0;
-        int min_ans = nums[n];
-        for(int i = n; i >= 0; --i){
-            min_ans = min(min_ans, nums[i]);
-            if(min_ans < nums[i])
-                ans++;
+        for(int i = 1; i < nums.size(); ++i){
+
+            if(nums[i] < nums[i - 1]){
+            if(++count > 1)
+                return false;
+            if(i == 1 || nums[i - 2] <= nums[i])
+                nums[i - 1] = nums[i];
+            else
+                nums[i] = nums[i - 1];
+            }
+
         }
         
         
-        return min(ans, count) > 1 ? false : true;
+        return true;
     }
 };
