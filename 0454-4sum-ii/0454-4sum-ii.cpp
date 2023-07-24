756ee3375145ab8184d2bd153cf363c51b5674b8
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-07-24 23:56:56 +0530

    Time: 637 ms (5.04%), Space: 55.8 MB (5.04%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int count = 0;
+        map<int, int> hash;
+        int n = nums1.size();
+        vector <int> a1;
+        vector <int> a2;
+        //making a combination of the first two vectors nums1 & nums2
+        for(int i = 0; i  < n; ++i)
+            for(int j = 0; j < n; ++j)
+                a1.push_back(nums1[i] + nums2[j]);
+        //making a combination of the last two vectors nums3 & nums4
+        for(int i = 0; i  < n; ++i)
+            for(int j = 0; j < n; ++j)
+                hash[nums3[i] + nums4[j]] += 1;
+        
+        for(int i = 0; i  < a1.size(); ++i)
+                if(hash[-a1[i]] != 0)
+                    count += hash[-a1[i]] ;
+        return count;
+                
+    }
+};
