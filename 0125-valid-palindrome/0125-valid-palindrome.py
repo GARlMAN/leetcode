df3c5f091a21fcf1cd2551b21f7afbc590ea4cf5
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-07-26 01:00:18 +0530

    Time: 73 ms (26.06%), Space: 17.1 MB (71.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        ans = ""
-        for i in s:
-            if i.isalpha() or i.isnumeric():
-                ans += i.lower()
-        print(ans)
-        if(ans == ans[::-1]):
-            return True
-        else:
-            return False
         
+        ans = ''
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                ans += i.lower()
+        return ans == ans[::-1]
