cffee16c6af3cc1a96bc182bfa5d85725de1a5c7
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-08-08 00:20:11 +0530

    Time: 28 ms (94.05%), Space: 12.3 MB (76.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& grid, int i, int j){
-        if(i >= grid.size() || j >= grid[i].size() || i < 0  || j < 0 || grid[i][j] == '0' || grid[i][j] == 'x' )
+        if(i == grid.size() || j == grid[0].size() || j == -1 || i == -1 || grid[i][j] != '1')
             return;
-        grid[i][j] = 'x';
+        grid[i][j] = 'X';
         solve(grid, i + 1, j);
         solve(grid, i - 1, j);
-        solve(grid, i, j + 1);
+        solve(grid, i , j +1);
         solve(grid, i, j - 1);
-        return;
     }
     int numIslands(vector<vector<char>>& grid) {
         int ans = 0;
-        for(int i = 0; i < grid.size(); ++i){
-            for(int j = 0; j < grid[i].size(); ++j){
-                if(grid[i][j] == '1'){
-                    ans += 1;
-                    solve(grid, i, j);
-                }
-                    
-            }
-        }
+        for(int i = 0; i < grid.size(); ++i)
+            for(int j = 0; j < grid[0].size(); ++j)
+                if(grid[i][j] == '1')
+                    ans += 1, solve(grid, i, j);
         return ans;
     }
 };
