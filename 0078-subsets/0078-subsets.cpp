5e54f166deccacc70005efa214ac0ffb8488e9a0
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-08-10 20:44:05 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (51.48%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> a;
+        ans.push_back(a);
+        solve(nums, a, 0);
+        return ans;
+    }
+    void solve(vector<int>& nums, vector<int> a, int k) {
+        if(k == nums.size())
+            return;
+        for(int i = k; i < nums.size(); ++i){
+            a.push_back(nums[i]);
+            ans.push_back(a);
+            solve(nums, a, i + 1);
+            a.pop_back();
+        }
+    }
+};
