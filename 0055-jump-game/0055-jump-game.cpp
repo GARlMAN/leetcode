8450e171ef82a1c13d1ed8cb30951996676895eb
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-08-19 15:48:06 +0530

    Time: 43 ms (92.88%), Space: 48.5 MB (42.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int ans = 0;
         int n = nums.size() - 1;
         int curr = n;
-        if(n == 0)
-            return true;
         for(int i = n - 1; i >= 0; --i){
-            cout << i + nums[i] << " " << curr;
             if(i + nums[i] >= curr)
                 curr = i;
         }
