2a80199cc2f2ed3e34c2a91563d56d5a079d20d3
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-08-23 15:05:27 +0530

    Time: 11 ms (74.19%), Space: 10 MB (81.41%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board, int m, int n){
-        if(m == board.size() || n == board[m].size())
+    void solve(vector<vector<char>>& grid, int i, int j){
+        if(i == grid.size() || j == grid[0].size() || j == -1 || i == -1 || grid[i][j] == '1' || grid[i][j] == 'X')
             return;
-        cout << m << " " << n << endl;
-        if(board[m][n] == 'O')
-            board[m][n] = '1';
-        if(m != 0 && board[m - 1][n] == 'O'){
-            solve(board, m - 1, n);
-        }
-        if(m != board.size() - 1 && board[m + 1][n] == 'O')
-            solve(board, m + 1, n);
-
-        if(n != board[m].size() - 1 && board[m][n + 1] == 'O')
-            solve(board, m, n + 1);
-            
-        if(n != 0 && board[m][n - 1] == 'O')
-            solve(board, m, n - 1);
-  
+        grid[i][j] = '1';
         
-        
-        
+        solve(grid, i + 1, j);
+        solve(grid, i - 1, j);
+        solve(grid, i , j +1);
+        solve(grid, i, j - 1);
     }
     
-    
-    
-    
-    
     void solve(vector<vector<char>>& board) {
-        
-
+       
         for(int m = 0; m < board.size(); ++m){
             for(int n = 0; n < board[m].size(); ++n){
                 if(m == 0 || n == 0 || m == board.size() - 1 ||n == board[m].size() - 1){
