1f6811c21205f8cc5e464d7616d1eba870824d98
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-08-30 00:11:45 +0530

    Time: 124 ms (54.91%), Space: 128.1 MB (52.50%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int total = 0;
-   
-        // find out where to start
-        int tank = 0;
-        int start = 0;
-        for (int i = 0; i < gas.size();i++) {
+        int tank = 0, total = 0, start = 0;
+        for(int i = 0; i < gas.size(); ++i){
             tank += gas[i] - cost[i];
             total += gas[i] - cost[i];
-            if (tank < 0) {
+            if(tank < 0){
                 start = i + 1;
-                tank = 0;
+                tank = 0; 
             }
         }
         return total < 0 ? -1 : start;
     }
-    
 };
