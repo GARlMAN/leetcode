ae63c0de0614787d30afc46d20664bb09dd60744
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-09-01 19:39:19 +0530

    Time: 41 ms (66.52%), Space: 16.5 MB (32.16%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,17 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [0]*len(s)
+        dp = [-1]*len(s)
 
         def dfs(i):
             if(i >= len(s)):
                 return 1
-            if(dp[i]):
+            if(dp[i] != -1):
                 return dp[i]
             temp1 = int(s[i : i + 1])
             temp2 = int(s[i : i + 2])
             ans = 0
             if(not temp1):
-                return 0
+                dp[i] = ans
+                return ans
             ans += dfs(i + 1)
             if(temp1 != temp2 and temp2 <= 26):
                 ans += dfs(i + 2)
