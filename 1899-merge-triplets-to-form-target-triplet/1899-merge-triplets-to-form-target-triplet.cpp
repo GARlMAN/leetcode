1e6de65d60a1e2673499cb97e29b196d7e27e28b
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-09-06 00:48:16 +0530

    Time: 455 ms (49.37%), Space: 163.7 MB (10.07%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        vector<int> comp;
+        vector<int> comp = {0,0,0};
         for(auto vec : triplets){
             if(target[0] >= vec[0] && target[1] >= vec[1] && target[2] >= vec[2]){
                 if(!comp.size())
                     comp = vec;
                 else
-                    comp[0] = max(vec[0], comp[0]), comp[1] = max(vec[1], comp[1]), comp[2] = max(vec[2], comp[2]);
+                    comp = {max(vec[0], comp[0]), max(vec[1], comp[1]), max(vec[2], comp[2])};
+                
                     
             }
         }
