c62b1654d140e95764f678959206399a4715f45c
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-09-09 19:05:04 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (24.54%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -3,25 +3,30 @@ public:
     vector<int> partitionLabels(string s) {
         int hash[30] = {0};
         vector<int> ans;
-        
+        //hasing all vlues
         for(auto ch : s)
             hash[ch - 'a'] += 1;
         
         int i = 0;
+        //interating through all values
         while(i < s.size()){
+            //new hash map to see if an element has been used before or not
             int arr[30] = {0}; 
             int count = 0;
             string temp = "";
             do{
                 temp += s[i];
+                //if it's a new element adding count
                 if(0 == arr[s[i] - 'a']++)
                     count++;
+                //if all types have been used decrementing count
                 if(0 == --hash[s[i] - 'a'])
                     count--;
                 i++;    
-            }while(count > 0);
+            }while(count > 0); //this for the final condition when the value of uiqueu elements is zero
             ans.push_back(temp.size());
         }
+        //returning the answer
         return ans;
         
     }
