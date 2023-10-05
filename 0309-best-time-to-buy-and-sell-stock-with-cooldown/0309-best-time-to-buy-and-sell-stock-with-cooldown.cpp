4789843c4ef8f98af1d45dfde075e38e31820dbc
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-10-05 20:45:15 +0530

    Time: 11 ms (9.56%), Space: 14.9 MB (5.02%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -10,7 +10,8 @@ public:
         if(i >= prices.size())
             return 0;
         if(dp[{i, buying}])
-            return dp[{i, buying}];
+            return  dp[{i, buying}] == -1 ? 0 : dp[{i, buying}];;
+        
         int notbuying = solve(prices, i + 1, buying);
         if(buying){
             int buy = solve(prices, i + 1, !buying) - prices[i];
@@ -21,6 +22,7 @@ public:
             int sell = solve(prices, i + 2, !buying) + prices[i];
             dp[{i, buying}] = max(notbuying, sell);
         }
-        return dp[{i, buying}];
+        dp[{i, buying}] = dp[{i, buying}] == 0 ? -1 : dp[{i, buying}];
+        return dp[{i, buying}] == -1 ? 0 : dp[{i, buying}];
     }
 };
