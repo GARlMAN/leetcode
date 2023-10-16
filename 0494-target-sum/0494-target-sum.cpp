5f2a617d0a032b3b95c65c8316861aae2aa4e8d3
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-10-16 23:05:21 +0530

    Time: 1102 ms (16.45%), Space: 9.3 MB (70.67%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
+    int count = 0;
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        return solve(0, target, 0, nums);;
+        solve(0, target, 0, nums);
+        return count;
     }
     
-    map<pair<int, int>, int> dp;
-     int solve(int sum, int target, int j, vector<int> &nums){
+    
+     void solve(int sum, int target, int j, vector<int> &nums){
         if(j == nums.size()){
             if(target == sum)
-                return 1;
-            return 0; 
+                count++;
+            return; 
             }
-         if(dp[{j, sum}])
-             return dp[{j, sum}] == -1 ? 0 : dp[{j, sum}] ;
-         int a = solve(sum + nums[j], target, j+1, nums);
-         int b = solve(sum - nums[j], target, j+1, nums);
-         
-         if(a + b == 0){
-             dp[{j, sum}] = -1;
-             return 0;
-         }
-
-             
-         return dp[{j, sum}] = a + b;
+         solve(sum + nums[j], target, j+1, nums);
+         solve(sum - nums[j], target, j+1, nums);
     }
 };
