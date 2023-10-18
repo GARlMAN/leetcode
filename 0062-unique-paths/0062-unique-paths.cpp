b7529fe9a0717769154f5ec56e46c15b214f87d4
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2023-10-18 16:41:46 +0530

    Time: 3 ms (27.89%), Space: 7 MB (8.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,9 +4,8 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, 1));
         
         //checking values for all values
-        for(int i = 0; i < m; ++i){
-            for(int j = 0; j < n; ++j){
-                if(j != 0 && i != 0)
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
