14d3e68a928275f5bd95828a40f2ca77165f2f49
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2024-04-11 16:28:58 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (96.13%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maxDepth(string s) {
         int ans = 0;
-        stack<int> st;
-        st.push(0);
+        int val = 0;
         for(auto ch : s)
             if(ch == '(')
-                st.push(st.top() + 1);
+                val++;
             else if(ch == ')')
-                ans = max(ans, st.top()), st.pop();
+                ans = max(ans, val--);
         return ans;
     }
 };
