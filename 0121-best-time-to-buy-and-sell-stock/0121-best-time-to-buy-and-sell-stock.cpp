3caf5d3c42d9ea06bc16b6bbed1ac4c42eb8e52e
GARlMAN <104691108+GARlMAN@users.noreply.github.com>
2024-05-30 16:05:36 +0530

    Time: 75 ms (90.79%), Space: 95.6 MB (93.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_val = prices[0];
-        int ans = 0;
-        for(auto num : prices)
-            min_val = min(num, min_val), ans = max(num - min_val, ans);
+        int low = prices[0], ans = 0;
+        for(int i = 0; i < prices.size(); ++i){
+            low = min(low, prices[i]);
+            ans = max(ans, prices[i] - low);
+        }
         return ans;
     }
 };
